3ac6248f9603d31e63c7f8bf26172189371fd127
Vidhanth Bafna <vidhanthbafna@gmail.com>
2016-04-03 09:33:55 +0530

    Aosp bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1157627904
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5912772608
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_PROP += device/motorola/titan/system.prop
 
 # inherit from the proprietary version
 -include vendor/motorola/titan/BoardConfigVendor.mk
